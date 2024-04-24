d827c11ff36830a58ba5e9ebbc43934094715beb
Abrar rizvi <abrar8949@gmail.com>
2024-04-24 15:23:31 -0700

    uploading

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// creating word counter
+import inquirer from "inquirer";
+console.log("WORD COUNTER"); // showing that coming program is for word counter
+const response = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter your Sentence",
+});
+// Now storing the input of user by removing white spaces and breaking into small pieces 
+const resOfAns = response["sentence"].trim().split(" ");
+// it shows how many words in your sentence which you write
+console.log(`your sentence is consists of ${resOfAns.length} words`);
